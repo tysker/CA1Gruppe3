3590437150e0a67d78fc4afeaf47bc325f91c641
Jörg <oerteljoerg@gmail.com>
2019-02-15 07:29:33 +0100

    DOM Manipulation

--- target/CA1-1.0-SNAPSHOT/JavaScript/DomManipulation.js
+++ target/CA1-1.0-SNAPSHOT/JavaScript/DomManipulation.js
@@ -4,7 +4,106 @@
  * and open the template in the editor.
  */
 
-var boys = ["Peter", "lars", "Ole"];
-var girls = ["Janne","hanne","Sanne"];
+var boys = ["Peter", "Lars", "Ole"];
+var girls = ["Janne", "Hanne", "Sanne"];
+
+
+
+function showArrays() {
+    // show all the boys
+    document.getElementById('boys').innerHTML = boys.join('<br>')
+
+// show all the girls
+    document.getElementById('girls').innerHTML = girls.join('<br>');
+
+// show all
+    document.getElementById('all').innerHTML = boys.concat(girls).join('<br>');
+}
+
+
+
+// Add a boy to the list
+document.getElementById('addboy').addEventListener('click', function () {
+    var newBoy = document.getElementById('newboy').value;
+    boys.push(newBoy);
+    // clear input field
+    document.getElementById('newboy').value = '';
+    //update List
+    showArrays();
+});
+
+// Add a girl to the list
+document.getElementById('addgirl').addEventListener('click', function () {
+    var newGirl = document.getElementById('newgirl').value;
+    girls.push(newGirl);
+    // clear input field
+    document.getElementById('newgirl').value = '';
+    //update List
+    showArrays();
+});
+
+// Remove a boy from the top of the List
+document.getElementById('removeboy').addEventListener('click', function () {
+    let first = document.getElementById('first').checked;
+    let last = document.getElementById('last').checked;
+    // check which radio button is clicked, otherwise the top of the list is removed
+    if (first === false) {
+        boys.pop();
+    } else if (last === false) {
+        boys.shift();
+    }
+    showArrays();
+});
+
+// Remove a girl from the top of the List
+document.getElementById('removegirl').addEventListener('click', function () {
+    let first = document.getElementById('first').checked;
+    let last = document.getElementById('last').checked;
+    // check which radio button is clicked, otherwise the top of the list is removed
+    if (first === false) {
+        girls.pop();
+    } else if (last === false) {
+        girls.shift();
+    }
+    showArrays();
+});
+
+
+showArrays();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
