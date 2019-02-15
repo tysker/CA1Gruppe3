a0ea120c1cab151aa27f054d63eeb14231da9d39
Jörg <oerteljoerg@gmail.com>
2019-02-15 22:26:40 +0100

    IFFE Jörg

--- target/CA1-1.0-SNAPSHOT/JavaScript/page3.js
+++ target/CA1-1.0-SNAPSHOT/JavaScript/page3.js
@@ -4,128 +4,177 @@
  * and open the template in the editor.
  */
 
-var boys = ["Peter", "Lars", "ole"];
-var girls = ["Janne", "hanne", "Sanne"];
-var toggleAllSort = true;
-var allList = document.getElementById("all");
+var listController = (function () {
 
 
-function convertToHTML(string) {
-    return "<li>" + string + "</li>";
-}
-
-function generateList(stringArray) {
-    var html = "";
-    for (let index = 0; index < stringArray.length; index++) {
-        html += convertToHTML(stringArray[index]);
-    }
-    return html;
-}
-
-
-function showArrays() {
-    // show all the boys
-    document.getElementById('boys').innerHTML = generateList(boys);
-    // show all the girls
-    document.getElementById('girls').innerHTML = generateList(girls);
-    // show all
-    document.getElementById('all').innerHTML = generateList(boys.concat(girls));
-}
-
-
-
-// Add a boy to the list
-document.getElementById('addboy').addEventListener('click', function () {
-    var newBoy = document.getElementById('newboy').value;
-    if (newBoy.length > 0) {
-        boys.push(newBoy);
-        // clear input field
-        document.getElementById('newboy').value = '';
-        //update List
-        showArrays();
+    function convertToHTML(string) {
+        return "<li>" + string + "</li>";
     }
 
-});
 
-// Add a girl to the list
-document.getElementById('addgirl').addEventListener('click', function () {
-    var newGirl = document.getElementById('newgirl').value;
-    if (newGirl.length > 0) {
-        girls.push(newGirl);
-        // clear input field
-        document.getElementById('newgirl').value = '';
-        //update List
-        showArrays();
-    }
-});
+    return {
+        generateList: function (stringArray) {
+            var html = "";
+            for (let index = 0; index < stringArray.length; index++) {
+                html += convertToHTML(stringArray[index]);
+            }
+            return html;
+        },
 
-// Remove a boy from the top of the List
-document.getElementById('removeboy').addEventListener('click', function () {
-    let first = document.getElementById('first').checked;
-    let last = document.getElementById('last').checked;
-    // check which radio button is clicked, otherwise the top of the list is removed
-    if (first === false) {
-        boys.pop();
-    } else if (last === false) {
-        boys.shift();
-    }
-    showArrays();
-});
-
-// Remove a girl from the top of the List
-document.getElementById('removegirl').addEventListener('click', function () {
-    let first = document.getElementById('first').checked;
-    let last = document.getElementById('last').checked;
-    // check which radio button is clicked, otherwise the top of the list is removed
-    if (first === false) {
-        girls.pop();
-    } else if (last === false) {
-        girls.shift();
-    }
-    showArrays();
-});
-
-// Reverse all names in the "ALL" coumn
-document.getElementById('reverse').addEventListener('click', function () {
-    var concatList = boys.concat(girls).reverse();
-    document.getElementById('all').innerHTML = generateList(concatList);
-});
-
-
-// function sortALL
-document.getElementById('sort').addEventListener('click', function () {
-    var concatList = boys.concat(girls);
-    descending(concatList);
-    if (!toggleAllSort) {
-        concatList = concatList.reverse();
-    }
-    allList.innerHTML = generateList(concatList);
-    toggleAllSort = !toggleAllSort;
-});
-
-
-function descending(array) {
-    array.sort(function (a, b) {
-        var nameA = a.toUpperCase(); // ignore upper and lowercase
-        var nameB = b.toUpperCase(); // ignore upper and lowercase
-        if (nameA < nameB) {
-            return -1;
+        descending: function (array) {
+            array.sort(function (a, b) {
+                var nameA = a.toUpperCase(); // ignore upper and lowercase
+                var nameB = b.toUpperCase(); // ignore upper and lowercase
+                if (nameA < nameB) {
+                    return -1;
+                }
+                if (nameA > nameB) {
+                    return 1;
+                }
+                // names must be equal
+                return 0;
+            });
         }
-        if (nameA > nameB) {
-            return 1;
+    }
+
+
+})();
+
+// UI Controller MODEL
+var UIController = (function () {
+
+    var DOMstrings = {
+        boysList: 'boys',
+        girlsList: 'girls',
+        allList: 'all',
+        addboy: 'addboy',
+        newboy: 'newboy',
+        addgirl: 'addgirl',
+        newgirl: 'newgirl',
+        removeboy: 'removeboy',
+        removegirl: 'removegirl',
+        firstRadioBtn: 'first',
+        lastRadioBtn: 'last',
+        revert: 'reverse',
+        sortList: 'sort'
+    };
+
+    var DOMLists = {
+        boys: ["Peter", "Lars", "ole"],
+        girls: ["Janne", "hanne", "Sanne"],
+        toggle: toggleAllSort = true
+
+    };
+
+    return {
+        getDOMstrings: function () {
+            return DOMstrings;
+        },
+
+        getDOMLists: function () {
+            return DOMLists;
         }
-        // names must be equal
-        return 0;
+    }
+})();
+
+
+// Global APP Controller. Is used to interact between ListController og UI Controller
+var controller = (function (listCtrl, UICtrl) {
+
+    var DOM = UICtrl.getDOMstrings();
+    var DOMList = UICtrl.getDOMLists();
+    var allList = document.getElementById(DOM.allList);
+
+
+    // Add a boy to the list
+    document.getElementById(DOM.addboy).addEventListener('click', function () {
+        var newBoy = document.getElementById(DOM.newboy).value;
+        if (newBoy.length > 0) {
+            DOMList.boys.push(newBoy);
+            // clear input field
+            document.getElementById(DOM.newboy).value = '';
+            //update List
+            controller.init();
+        }
     });
 
-}
+    // Add a girl to the list
+    document.getElementById(DOM.addgirl).addEventListener('click', function () {
+        var newGirl = document.getElementById(DOM.newgirl).value;
+        if (newGirl.length > 0) {
+            DOMList.girls.push(newGirl);
+            // clear input field
+            document.getElementById(DOM.newgirl).value = '';
+            //update List
+            controller.init();
+        }
+    });
+
+    // Remove a boy from the top of the List
+    document.getElementById(DOM.removeboy).addEventListener('click', function () {
+        let first = document.getElementById(DOM.firstRadioBtn).checked;
+        let last = document.getElementById(DOM.lastRadioBtn).checked;
+        // check which radio button is clicked, otherwise the top of the list is removed
+        if (first === false) {
+            DOMList.boys.pop();
+        } else if (last === false) {
+            DOMList.boys.shift();
+        }
+        controller.init();
+    });
+
+    // Remove a girl from the top of the List
+    document.getElementById(DOM.removegirl).addEventListener('click', function () {
+        let first = document.getElementById(DOM.firstRadioBtn).checked;
+        let last = document.getElementById(DOM.lastRadioBtn).checked;
+        // check which radio button is clicked, otherwise the top of the list is removed
+        if (first === false) {
+            DOMList.girls.pop();
+        } else if (last === false) {
+            DOMList.girls.shift();
+        }
+        controller.init();
+    });
+
+    // Reverse all names in the "ALL" coumn
+    document.getElementById(DOM.revert).addEventListener('click', function () {
+        var concatList = DOMList.boys.concat(DOMList.girls).reverse();
+        document.getElementById(DOM.allList).innerHTML = listCtrl.generateList(concatList);
+    });
+
+
+    // function sortALL
+    document.getElementById(DOM.sortList).addEventListener('click', function () {
+        var concatList = DOMList.boys.concat(DOMList.girls);
+        listCtrl.descending(concatList);
+        if (!DOMList.toggleAllSort) {
+            concatList = concatList.reverse();
+        }
+        allList.innerHTML = listCtrl.generateList(concatList);
+        DOMList.toggleAllSort = !DOMList.toggleAllSort;
+    });
+
+    return {
+        init: function () {
+            // show all the boys
+            document.getElementById(DOM.boysList).innerHTML = listCtrl.generateList(DOMList.boys);
+            // show all the girls
+            document.getElementById(DOM.girlsList).innerHTML = listCtrl.generateList(DOMList.girls);
+            // show all
+            document.getElementById(DOM.allList).innerHTML = listCtrl.generateList(DOMList.boys.concat(DOMList.girls));
+        }
+    };
+
+})(listController, UIController);
+
+
+// initialize Program
+controller.init();
 
 
 
 
 
-
-showArrays();
 
 
 
